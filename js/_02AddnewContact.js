45ec1f132b5ca4973229e8031da8861ecdef1689
Raj143 <rajshingade777@gmail.com>
2024-04-29 01:32:05 +0530

    [Raj] add :actual code for adding new contact to the addressbook

--- js/_02AddnewContact.js
+++ js/_02AddnewContact.js
@@ -1,39 +1,12 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-const readline = __importStar(require("readline"));
-const rl = readline.createInterface({
+var readline = require("readline");
+var rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
-rl.question('Enter something', (answer) => {
-    console.log('your anse', answer);
-    rl.close();
-});
-class Contact {
-    constructor(firstName, lastName, address, city, state, zip, phone, email) {
+var Contact = /** @class */ (function () {
+    function Contact(firstName, lastName, address, city, state, zip, phone, email) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.address = address;
@@ -42,21 +15,49 @@ class Contact {
         this.zip = zip;
         this.phone = phone;
         this.email = email;
-        console.log("Hello Guys!! Welcome to addressbook");
     }
-}
-class Addressbook {
-    constructor() {
+    return Contact;
+}());
+var Addressbook = /** @class */ (function () {
+    function Addressbook() {
         this.contacts = [];
     }
-    addContact(contact) {
+    Addressbook.prototype.addContact = function (contact) {
         this.contacts.push(contact);
-    }
-    viewContacts() {
-        this.contacts.forEach(contact => console.log(contact));
-    }
+    };
+    Addressbook.prototype.viewContacts = function () {
+        this.contacts.forEach(function (contact) { return console.log(contact); });
+    };
+    return Addressbook;
+}());
+function promptForContactDetails() {
+    rl.question('Enter first name: ', function (firstName) {
+        rl.question('Enter last name: ', function (lastName) {
+            rl.question('Enter address: ', function (address) {
+                rl.question('Enter city: ', function (city) {
+                    rl.question('Enter state: ', function (state) {
+                        rl.question('Enter zip: ', function (zip) {
+                            rl.question('Enter phone: ', function (phone) {
+                                rl.question('Enter email: ', function (email) {
+                                    var contact = new Contact(firstName, lastName, address, city, state, zip, phone, email);
+                                    addressBook.addContact(contact);
+                                    rl.question('Do you want to add another contact? (yes/no): ', function (answer) {
+                                        if (answer.toLowerCase() === 'yes') {
+                                            promptForContactDetails();
+                                        }
+                                        else {
+                                            rl.close();
+                                            addressBook.viewContacts();
+                                        }
+                                    });
+                                });
+                            });
+                        });
+                    });
+                });
+            });
+        });
+    });
 }
-let contact = new Contact("raj", "shingade", "Jai Bhavani nagar", "Aurangabad", "Maharashtra", "232323", "9878789064", "raj@shignade.com");
-let addressBook = new Addressbook();
-addressBook.addContact(contact);
-addressBook.viewContacts();
+var addressBook = new Addressbook();
+promptForContactDetails();
