b8d59780cc01cee897e4d7ad3adc875b76d302e3
Raj143 <rajshingade777@gmail.com>
2024-04-29 02:13:04 +0530

    [RAj] add: adding the file delete contact by name

---
+++ js/_04Delete_contact_byname.js
@@ -0,0 +1,139 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var readline = require("readline");
+var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+var Contact = /** @class */ (function () {
+    function Contact(firstName, lastName, address, city, state, zip, phone, email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phone = phone;
+        this.email = email;
+    }
+    return Contact;
+}());
+var Addressbook = /** @class */ (function () {
+    function Addressbook() {
+        this.contacts = [];
+    }
+    Addressbook.prototype.addContact = function (contact) {
+        this.contacts.push(contact);
+    };
+    Addressbook.prototype.viewContacts = function () {
+        this.contacts.forEach(function (contact) { return console.log(contact); });
+    };
+    Addressbook.prototype.findContactByName = function (firstName, lastName) {
+        return this.contacts.find(function (contact) { return contact.firstName === firstName && contact.lastName === lastName; });
+    };
+    Addressbook.prototype.deleteContact = function (firstName, lastName) {
+        var index = this.contacts.findIndex(function (contact) { return contact.firstName === firstName && contact.lastName === lastName; });
+        if (index !== -1) {
+            this.contacts.splice(index, 1);
+            console.log('Contact deleted successfully.');
+        }
+        else {
+            console.log('Contact not found.');
+        }
+    };
+    Addressbook.prototype.editContact = function (firstName, lastName) {
+        var contact = this.findContactByName(firstName, lastName);
+        if (contact) {
+            rl.question('Enter new address: ', function (address) {
+                rl.question('Enter new city: ', function (city) {
+                    rl.question('Enter new state: ', function (state) {
+                        rl.question('Enter new zip: ', function (zip) {
+                            rl.question('Enter new phone: ', function (phone) {
+                                rl.question('Enter new email: ', function (email) {
+                                    contact.address = address;
+                                    contact.city = city;
+                                    contact.state = state;
+                                    contact.zip = zip;
+                                    contact.phone = phone;
+                                    contact.email = email;
+                                    console.log('Contact details updated successfully.');
+                                    promptForEdit(); // After updating, prompt again for further actions
+                                });
+                            });
+                        });
+                    });
+                });
+            });
+        }
+        else {
+            console.log('Contact not found.');
+            promptForEdit();
+        }
+    };
+    return Addressbook;
+}());
+var addressBook = new Addressbook();
+function promptForContactDetails() {
+    rl.question('Enter first name: ', function (firstName) {
+        rl.question('Enter last name: ', function (lastName) {
+            rl.question('Enter address: ', function (address) {
+                rl.question('Enter city: ', function (city) {
+                    rl.question('Enter state: ', function (state) {
+                        rl.question('Enter zip: ', function (zip) {
+                            rl.question('Enter phone: ', function (phone) {
+                                rl.question('Enter email: ', function (email) {
+                                    var contact = new Contact(firstName, lastName, address, city, state, zip, phone, email);
+                                    addressBook.addContact(contact);
+                                    rl.question('Do you want to add another contact? (yes/no): ', function (answer) {
+                                        if (answer.toLowerCase() === 'yes') {
+                                            promptForContactDetails();
+                                        }
+                                        else {
+                                            rl.close();
+                                            addressBook.viewContacts();
+                                            promptForEditOrDelete();
+                                        }
+                                    });
+                                });
+                            });
+                        });
+                    });
+                });
+            });
+        });
+    });
+}
+function promptForEditOrDelete() {
+    rl.question('Do you want to edit or delete a contact? (edit/delete/exit): ', function (answer) {
+        if (answer.toLowerCase() === 'edit') {
+            promptForEdit();
+        }
+        else if (answer.toLowerCase() === 'delete') {
+            promptForDelete();
+        }
+        else if (answer.toLowerCase() === 'exit') {
+            console.log('Exiting...');
+            rl.close();
+        }
+        else {
+            console.log('Invalid option. Please try again.');
+            promptForEditOrDelete();
+        }
+    });
+}
+function promptForEdit() {
+    rl.question('Enter first name of the contact you want to edit: ', function (firstName) {
+        rl.question('Enter last name of the contact you want to edit: ', function (lastName) {
+            addressBook.editContact(firstName, lastName);
+        });
+    });
+}
+function promptForDelete() {
+    rl.question('Enter first name of the contact you want to delete: ', function (firstName) {
+        rl.question('Enter last name of the contact you want to delete: ', function (lastName) {
+            addressBook.deleteContact(firstName, lastName);
+            promptForEditOrDelete();
+        });
+    });
+}
+promptForContactDetails();
---
+++ src/_04Delete_contact_byname.ts
@@ -0,0 +1,140 @@
+import * as readline from 'readline';
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+class Contact {
+    constructor(
+        public firstName: string,
+        public lastName: string,
+        public address: string,
+        public city: string,
+        public state: string,
+        public zip: string,
+        public phone: string,
+        public email: string
+    ) {}
+}
+
+class Addressbook {
+    public contacts: Contact[] = [];
+
+    public addContact(contact: Contact) {
+        this.contacts.push(contact);
+    }
+
+    public viewContacts(): void {
+        this.contacts.forEach(contact => console.log(contact));
+    }
+
+    public findContactByName(firstName: string, lastName: string): Contact | undefined {
+        return this.contacts.find(contact => contact.firstName === firstName && contact.lastName === lastName);
+    }
+
+    public deleteContact(firstName: string, lastName: string): void {
+        const index = this.contacts.findIndex(contact => contact.firstName === firstName && contact.lastName === lastName);
+        if (index !== -1) {
+            this.contacts.splice(index, 1);
+            console.log('Contact deleted successfully.');
+        } else {
+            console.log('Contact not found.');
+        }
+    }
+    public editContact(firstName: string, lastName: string) {
+        const contact = this.findContactByName(firstName, lastName);
+        if (contact) {
+            rl.question('Enter new address: ', (address) => {
+                rl.question('Enter new city: ', (city) => {
+                    rl.question('Enter new state: ', (state) => {
+                        rl.question('Enter new zip: ', (zip) => {
+                            rl.question('Enter new phone: ', (phone) => {
+                                rl.question('Enter new email: ', (email) => {
+                                    contact.address = address;
+                                    contact.city = city;
+                                    contact.state = state;
+                                    contact.zip = zip;
+                                    contact.phone = phone;
+                                    contact.email = email;
+                                    console.log('Contact details updated successfully.');
+                                    promptForEdit(); // After updating, prompt again for further actions
+                                });
+                            });
+                        });
+                    });
+                });
+            });
+        } else {
+            console.log('Contact not found.');
+            promptForEdit(); 
+        }
+    }
+}
+
+let addressBook = new Addressbook();
+
+function promptForContactDetails() {
+    rl.question('Enter first name: ', (firstName) => {
+        rl.question('Enter last name: ', (lastName) => {
+            rl.question('Enter address: ', (address) => {
+                rl.question('Enter city: ', (city) => {
+                    rl.question('Enter state: ', (state) => {
+                        rl.question('Enter zip: ', (zip) => {
+                            rl.question('Enter phone: ', (phone) => {
+                                rl.question('Enter email: ', (email) => {
+                                    const contact = new Contact(firstName, lastName, address, city, state, zip, phone, email);
+                                    addressBook.addContact(contact);
+                                    rl.question('Do you want to add another contact? (yes/no): ', (answer) => {
+                                        if (answer.toLowerCase() === 'yes') {
+                                            promptForContactDetails();
+                                        } else {
+                                            rl.close();
+                                            addressBook.viewContacts();
+                                            promptForEditOrDelete();
+                                        }
+                                    });
+                                });
+                            });
+                        });
+                    });
+                });
+            });
+        });
+    });
+}
+
+function promptForEditOrDelete() {
+    rl.question('Do you want to edit or delete a contact? (edit/delete/exit): ', (answer) => {
+        if (answer.toLowerCase() === 'edit') {
+            promptForEdit();
+        } else if (answer.toLowerCase() === 'delete') {
+            promptForDelete();
+        } else if (answer.toLowerCase() === 'exit') {
+            console.log('Exiting...');
+            rl.close();
+        } else {
+            console.log('Invalid option. Please try again.');
+            promptForEditOrDelete();
+        }
+    });
+}
+
+function promptForEdit() {
+    rl.question('Enter first name of the contact you want to edit: ', (firstName) => {
+        rl.question('Enter last name of the contact you want to edit: ', (lastName) => {
+            addressBook.editContact(firstName, lastName);
+        });
+    });
+}
+
+function promptForDelete() {
+    rl.question('Enter first name of the contact you want to delete: ', (firstName) => {
+        rl.question('Enter last name of the contact you want to delete: ', (lastName) => {
+            addressBook.deleteContact(firstName, lastName);
+            promptForEditOrDelete();
+        });
+    });
+}
+
+promptForContactDetails();
